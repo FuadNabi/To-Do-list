97722dc994856428f75555c18f28cc811f23e24d
FuadNabi <fuad.nebiyev95@mail.ru>
2022-11-17 14:57:51 +0400

    Add test for modify

--- src/module/toDoDom.test.js
+++ src/module/toDoDom.test.js
@@ -4,6 +4,7 @@
 import toDoDom from './toDoDom.js';
 import Data from './data.js';
 import Remove from './remove.js';
+import Modify from './modify.js';
 
 describe('Testing add and remove', () => {
   test('Add function test', () => {
@@ -21,5 +22,18 @@ describe('Testing add and remove', () => {
     Remove.data(li);
 
     expect(li.length).toBe(undefined);
+  });
+
+  test('Editing function test', () => {
+    document.body.innerHTML = '<ul class="main-body-ul"></ul>';
+    const appList = document.querySelector('.main-body-ul');
+
+    toDoDom.data('task1', appList);
+    const text = document.querySelector('.data-text');
+    text.value = 'task2';
+    const data = document.querySelector('.main-body-li');
+    Modify.modify(data, text);
+
+    expect(text.value).toBe('task2');
   });
 });
