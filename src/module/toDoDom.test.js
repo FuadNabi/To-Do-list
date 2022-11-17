e5890e227627e648b78b776736eb055b06ebca07
FuadNabi <fuad.nebiyev95@mail.ru>
2022-11-17 11:39:49 +0400

    Add remove and add test

--- src/module/toDoDom.test.js
+++ src/module/toDoDom.test.js
@@ -3,6 +3,7 @@
  */
 import toDoDom from './toDoDom.js';
 import Data from './data.js';
+import Remove from './remove.js';
 
 describe('Testing add and remove', () => {
   test('Add function test', () => {
@@ -11,5 +12,14 @@ describe('Testing add and remove', () => {
     toDoDom.data('task1', appList);
 
     expect(Data.dataGet()[0].text).toEqual('task1');
+  });
+
+  test('Remove function test', () => {
+    document.body.innerHTML = '<ul class="main-body-ul"></ul>';
+    const li = document.createElement('li');
+    li.setAttribute('data-id', '1');
+    Remove.data(li);
+
+    expect(li.length).toBe(undefined);
   });
 });
